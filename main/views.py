ef70f8c6d61cc1b204a62160ec06a070fb6e5b8f
Dara <darazapri@gmail.com>
2024-09-04 13:47:29 +0700

    Menambahkan MVT 2

--- main/views.py
+++ main/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'npm' : '2306123456',
-        'name': 'Pak Bepe',
-        'class': 'PBP E'
+        'npm' : '2306165906',
+        'name': 'Dara Zakya Apriani',
+        'class': 'PBP B'
     }
 
     return render(request, "main.html", context)
