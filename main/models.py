3f26b1100d230d1de34dd0aec6f7a6ba9558295b
Dara <darazapri@gmail.com>
2024-09-11 14:20:42 +0700

    menambahkan ci/cd pws

--- main/models.py
+++ main/models.py
@@ -1,7 +1,9 @@
-# Create your models here.
+import uuid
 from django.db import models
 
+
 class MoodEntry(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     mood = models.CharField(max_length=255)
     time = models.DateField(auto_now_add=True)
     feelings = models.TextField()
