870637d953f10e05dac020b2db0fea286190397e
Dara <darazapri@gmail.com>
2024-09-25 14:34:28 +0700

    coba

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,7 @@ from main.views import show_main, create_mood_entry, edit_mood, delete_mood, reg
 
 app_name = 'main'
 
+
 urlpatterns = [
     path('', show_main, name='show_main'),
     path('create-mood-entry', create_mood_entry, name='create_mood_entry'),
