161bc6065314dc1306e2f1dcb70f3cf0b2dd8f18
Dara <darazapri@gmail.com>
2024-09-27 20:04:51 +0700

    tes

--- main/models.py
+++ main/models.py
@@ -2,7 +2,6 @@ import uuid
 from django.db import models
 from django.contrib.auth.models import User
 
-
 class MoodEntry(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     mood = models.CharField(max_length=255)
