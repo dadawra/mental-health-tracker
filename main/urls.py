67f52893bc6196b892bdf2be67307d2714b3c8db
Dara <darazapri@gmail.com>
2024-10-02 14:18:38 +0700

    menyelesaikan tutorial lab

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, create_mood_entry, edit_mood, delete_mood, register, login_user,  logout_user, show_xml, show_json, show_xml_by_id, show_json_by_id
+from main.views import show_main, create_mood_entry, edit_mood, delete_mood, register, login_user,  logout_user, show_xml, show_json, show_xml_by_id, show_json_by_id, add_mood_entry_ajax
 
 app_name = 'main'
 
@@ -15,5 +15,5 @@ urlpatterns = [
     path('json/', show_json, name='show_json'),
     path('xml/<str:id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/<str:id>/', show_json_by_id, name='show_json_by_id'),
-    
+    path('create-mood-entry-ajax', add_mood_entry_ajax, name='add_mood_entry_ajax'),
 ]
